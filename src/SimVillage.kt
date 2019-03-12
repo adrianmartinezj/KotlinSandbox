8a0e68922a0f63e05db1f914914d3225cab6e52f
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-03-12 15:03:22 -0700

    Lambda and anonymous functions

---
+++ src/SimVillage.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val greetingFunction: (String) -> String = { playerName ->
+        val currentYear = 2019
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+    println(greetingFunction("Guyal"))
+}
