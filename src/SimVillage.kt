d368911c9c5334eaa6ba2effa5b60e9af14679be
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-03-13 14:58:52 -0700

    Lambdas and function references

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,7 +1,24 @@
 fun main(args: Array<String>) {
-    val greetingFunction: (String) -> String = { playerName ->
-        val currentYear = 2019
-        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
-    }
+    runSimulation()
+}
+
+fun runSimulation(){
+    val greetingFunction = configureGreetingFunction()
     println(greetingFunction("Guyal"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return {playerName: String ->
+        val currentYear = 2019
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
+        "Welcome to SimVillage, $playerName (copyright $currentYear)"
+    }
+}
+
+fun printConstructionCost(numBuildings: Int){
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
 }
